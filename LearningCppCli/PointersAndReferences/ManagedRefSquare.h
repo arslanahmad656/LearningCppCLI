30ac45b745a37c0c933778eee60ac215131e2a57
Arslan Ahmad <ars.ahm6@gmail.com>
2019-07-18 18:35:58 +0500

    Ref class example added

--- LearningCppCli/PointersAndReferences/ManagedRefSquare.h
+++ LearningCppCli/PointersAndReferences/ManagedRefSquare.h
@@ -3,7 +3,7 @@ ref class ManagedRefSquare
 {
 	// Objects of ref classes cannot be passed by value. They can only be passed by reference.
 	// It seems logical because only those objects can be passed by value that can be created on the stack.
-	// Furthermore, there is no managed pointer, but only managed reference.
+	// Furthermore, there is no such thing as managed pointer, but only managed reference.
 private:
 	int side;
 public:
