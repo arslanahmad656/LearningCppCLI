ab43b66d86cdd16dbb409584689fe0ac57e47b45
Arslan Ahmad <ars.ahm6@gmail.com>
2019-07-24 15:03:56 +0500

    dsfdsfds

---
+++ LearningCppCli/IntroAgain/UsernameInfo.cpp
@@ -0,0 +1,29 @@
+// compile this class using CL /CLR /LD
+
+#pragma once
+
+#pragma comment(lib, "Advapi32")
+
+#include <windows.h>
+#include <lmcons.h>
+
+using namespace System;
+
+namespace ThisNamespace
+{
+	class UsernameInfo
+	{
+	public:
+		String^ GetUserInfo()
+		{
+			TCHAR buffer[UNLEN + 1];
+			DWORD length = UNLEN + 1;
+
+			GetUserName(buffer, &length);
+
+			String^ username = gcnew String(buffer);
+			return username;
+		}
+	};
+}
+
