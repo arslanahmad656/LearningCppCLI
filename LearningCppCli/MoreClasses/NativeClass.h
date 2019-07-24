24b8aaf0da5c9d6c5fb7adf5f2de6249841c7d4b
Arslan Ahmad <ars.ahm6@gmail.com>
2019-07-24 10:17:14 +0500

    Native class examples

--- LearningCppCli/MoreClasses/NativeClass.h
+++ LearningCppCli/MoreClasses/NativeClass.h
@@ -1,5 +1,11 @@
 #pragma once
+
+#include "ManagedRefClass.h"
+
 class NativeClass
 {
+private:
+	//ManagedRefClass^ managedClass;	// native classes cannot contain a handle
+	//ManagedRefClass managedClass;	// native classes just cannot contain managed types
 };
 
