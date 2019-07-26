a07ea65eca8b000d48af263494fceeccc9192481
Arslan Ahmad <ars.ahm6@gmail.com>
2019-07-26 19:00:28 +0500

    Copy constructor examples added

--- LearningCppCli/InstantiatingCliClasses/RefClassStudent.h
+++ LearningCppCli/InstantiatingCliClasses/RefClassStudent.h
@@ -7,11 +7,26 @@ ref class RefClassStudent
 private:
 	String^ name;
 	int rollNumber;
+	void Copy(RefClassStudent^ copy)
+	{
+		this->name = copy->name;
+		this->rollNumber = copy->rollNumber;
+	}
 public:
 	RefClassStudent(String^ name, int rollNumber)
 		: name(name), rollNumber(rollNumber)
 	{
 		// empty body
+	}
+
+	RefClassStudent(RefClassStudent^ copy) // this is the standard copy ctor
+	{
+		Copy(copy);
+	}
+
+	RefClassStudent(RefClassStudent% copy)	// non-standard version just for convenience in case of stack semantics
+	{
+		Copy(% copy);
 	}
 
 	String^ GetStudentInfo()
